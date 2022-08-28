57512d9a7c5e805cfce05f733e0c1d12294f45ec
aenz <andrewjensor@gmail.com>
2022-08-28 11:47:54 -0700

    working on search bar js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,18 @@
-const weatherAPIKey = "be0b7830ffc0880741abe868b09e6400"
-const googlePlacesAPIKey = "AIzaSyBvyumRmK4e7wI9QoM13TGkn9OA2Ibm51w"
+const weatherAPIKey = "be0b7830ffc0880741abe868b09e6400";
+const googlePlacesAPIKey = "AIzaSyBvyumRmK4e7wI9QoM13TGkn9OA2Ibm51w";
 
-let lat;
-let lon;
-
-function getCityInfo(lat, lon){
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=` + lat + `&lon=` + lon + `&appid=` + weatherAPIKey)
-
-        .then(function(response) {
-            return response.json()
-        })
-
-        .then(function(data) {
-            console.log(data)
-        })
-}
+let latitude;
+let longitude;
 
 
-function getCityCoord(){
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", apiGroup());
+
+const searchInputBox = document.getElementsByName("city-search-input");
+let cityInputText;
+
+
+function getCity(){
 
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=` + cityInputText +`&limit=1&appid=` + weatherAPIKey)
 
@@ -25,33 +20,63 @@ function getCityCoord(){
         return response.json()
     })
 
-    .then(function (data) {
-    lat = data.lat
-    lon = data.lon
+    .then(function(data) {
     console.log(data)
     })
-
     
+    // .then(fetch(`https://api.openweathermap.org/data/2.5/weather?lat=` + latitude + `&lon=` + longitude + `&appid=` + weatherAPIKey))
+
+    // .then(function(response) {
+    //     return response.json()
+    // })
+
+    // .then(function(data) {
+    //     console.log(data)
+    // })
+
+
+};
+
+// function getCityInfo(lat, lon){
+
+        
+
+//         .then(function(response) {
+//             return response.json()
+//         })
+
+//         .then(function(data) {
+//             console.log(data)
+//         })
+// };
+
+// const cityInputText = "london"
+
+// function getTeleportImage(){
+
+//     fetch(`https://api.teleport.org/api/urban_areas/slug:` + searchedCity)
+
+//     .then(function(response) {
+//         return response.json()
+//     })
+// };
+
+// function get5Day(){
+
+//     fetch(`api.openweathermap.org/data/2.5/forecast?lat=` + latitude + `&lon=` + longitude + `&appid=` + weatherAPIKey)
+// };
+
+function apiGroup(){
+    cityInputText = searchInputBox.value;
+    console.log("Button Clicked")
+    getCity()
+    console.log(latitude + "::" + longitude)
+
+    // get5Day()
+    // getCityInfo()
+    // getTeleportImage()
 }
 
 
-
-const cityInputText = "london"
-
-function getTeleportImage(){
-    fetch(`https://api.teleport.org/api/urban_areas/slug:` + searchedCity)
-
-    .then(function(response) {
-        return response.json()
-    })
-}
-
-function get5Day(){
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=` + lat + `&lon=` + lon + `&appid=` + weatherAPIKey)
-}
-
-getCityCoord()
 // getCityInfo(lat, lon)
-getCityInfo(51.5073219, -0.1276474)
-
-document.getElementById("search-btn")
+// getCityInfo(51.5073219, -0.1276474);
