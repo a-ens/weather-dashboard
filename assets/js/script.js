f1e97c1053d6848230dc9e072b0d57ce662bfdca
aenz <andrewjensor@gmail.com>
2022-08-29 12:41:49 -0700

    more js edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,37 @@ const googlePlacesAPIKey = "AIzaSyBvyumRmK4e7wI9QoM13TGkn9OA2Ibm51w";
 let latitude;
 let longitude;
 
-
-const searchBtn = document.getElementById("search-btn");
-searchBtn.addEventListener("click", apiGroup());
+let today = moment().format('L');
 
 const searchInputBox = document.getElementsByName("city-search-input");
-let cityInputText;
+let cityInputText = undefined
 
+
+
+function currentCondition(city) {
+
+    let apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=` + cityInputText +`&limit=1&appid=` + weatherAPIKey;
+
+    $.ajax({
+        url: apiCall,
+        method: "GET"
+    }).then(function(cityWeatherResponse) {
+        console.log(cityWeatherResponse);
+    })};
+
+
+    const handleErrors = (response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }
+
+    const currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
 
 function getCity(){
 
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=` + cityInputText +`&limit=1&appid=` + weatherAPIKey)
+    fetch()
 
     .then(function(response) {
         return response.json()
@@ -23,7 +43,7 @@ function getCity(){
     .then(function(data) {
     console.log(data)
     })
-    
+
     // .then(fetch(`https://api.openweathermap.org/data/2.5/weather?lat=` + latitude + `&lon=` + longitude + `&appid=` + weatherAPIKey))
 
     // .then(function(response) {
@@ -33,9 +53,27 @@ function getCity(){
     // .then(function(data) {
     //     console.log(data)
     // })
-
-
 };
+
+function apiGroup(){
+    let cityInputText = searchInputBox.value;
+    console.log("Button Clicked")
+    .then(searchInputBox.empty())
+    getCity()
+    console.log(latitude + "::" + longitude)
+
+    // get5Day()
+    // getCityInfo()
+    // getTeleportImage()
+};
+
+function submitted(event) {
+    event.preventDefault()
+    apiGroup
+};
+
+const searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click", submitted);
 
 // function getCityInfo(lat, lon){
 
@@ -65,18 +103,6 @@ function getCity(){
 
 //     fetch(`api.openweathermap.org/data/2.5/forecast?lat=` + latitude + `&lon=` + longitude + `&appid=` + weatherAPIKey)
 // };
-
-function apiGroup(){
-    cityInputText = searchInputBox.value;
-    console.log("Button Clicked")
-    getCity()
-    console.log(latitude + "::" + longitude)
-
-    // get5Day()
-    // getCityInfo()
-    // getTeleportImage()
-}
-
 
 // getCityInfo(lat, lon)
 // getCityInfo(51.5073219, -0.1276474);
