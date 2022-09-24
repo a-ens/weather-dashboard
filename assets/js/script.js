0671f82ebeabd7bfc7654753c025b25460bf67bb
aenz <andrewjensor@gmail.com>
2022-09-24 00:03:33 -0700

    list items appear now

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const weatherAPIKey = "be0b7830ffc0880741abe868b09e6400";
 const googlePlacesAPIKey = "AIzaSyBvyumRmK4e7wI9QoM13TGkn9OA2Ibm51w";
 
 let today = moment().format('L');
-let searchHistoryList = [];
+let searchHistory = [];
 
 const searchInputBox = document.getElementsByName("city-search-input");
 let cityInputText = undefined
@@ -10,21 +10,21 @@ let cityInputText = undefined
 
 function callWeather(city) {
 
-    let apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=` + cityInputText +`&limit=1&appid=` + weatherAPIKey;
+    let apiCall = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=` + weatherAPIKey;
 
     $.ajax({
         url: apiCall,
         method: "GET"
     }).then(function(firstWeatherResponse) {
         console.log(firstWeatherResponse);
-    })
+    
 
     $("").css("display", "block");
     $("#today-box").empty();
 
 
-    let latitude = firstWeatherResponse.coord.lat;
-    let longitude = firstWeatherResponse.coord.lon;
+    let latitude = firstWeatherResponse.lat;
+    let longitude = firstWeatherResponse.lon;
 
     let iconCode = firstWeatherResponse.weather[0].icon;
     let iconURL = `https://openweathermap.org/img/w/` + iconCode + `.png`;
@@ -46,37 +46,37 @@ function callWeather(city) {
 
     let uviCall = `https://api.openweathermap.org/data/2.5/uvi?lat=` + latitude + `&lon=` + longitude + `&appid=` + weatherAPIKey;
 
-    $.ajax({
-        url: uviCall,
-        method: "GET"
-    }).then(function(uviResponse) {
-        console.log(uviResponse);
+        $.ajax({
+            url: uviCall,
+            method: "GET"
+        }).then(function(uviResponse) {
+            console.log(uviResponse);
 
-        let uvIndex = uviResponse.value;
-        let uvIndexObject = $(`
-            <p>UV Index: 
-                <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
-            </p>
-        `);
+            let uvIndex = uviResponse.value;
+            let uvIndexObject = $(`
+                <p>UV Index: 
+                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+                </p>
+            `);
 
-        $('#today-box').append(uvIndexObject);
+            $('#today-box').append(uvIndexObject);
 
-        if (uvIndex >= 0 && uvIndex <= 2) {
-            $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
-        } else if (uvIndex >= 3 && uvIndex <= 5) {
-            $("#uvIndexColor").css("background-color", "#FFF300");
-        } else if (uvIndex >= 6 && uvIndex <= 7) {
-            $("#uvIndexColor").css("background-color", "#F18B00");
-        } else if (uvIndex >= 8 && uvIndex <= 10) {
-            $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
-        } else {
-            $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
-        };  
+            if (uvIndex >= 0 && uvIndex <= 2) {
+                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+            } else if (uvIndex >= 3 && uvIndex <= 5) {
+                $("#uvIndexColor").css("background-color", "#FFF300");
+            } else if (uvIndex >= 6 && uvIndex <= 7) {
+                $("#uvIndexColor").css("background-color", "#F18B00");
+            } else if (uvIndex >= 8 && uvIndex <= 10) {
+                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+            } else {
+                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+            };  
 
-        fiveDayForecast (latitude, longitude)
+            fiveDayForecast (latitude, longitude)
 
-    };
-
+        });
+    });
 }
 
 function fiveDayForecast (latitude, longitude){
@@ -91,25 +91,25 @@ function fiveDayForecast (latitude, longitude){
         $("#fiveDay").empty();
         
         for (let i = 1; i < 6; i++) {
-            var cityInfo = {
+            let cityInfo = {
                 date: fiveDayResponse.daily[i].dt,
                 icon: fiveDayResponse.daily[i].weather[0].icon,
                 temp: fiveDayResponse.daily[i].temp.day,
                 humidity: fiveDayResponse.daily[i].humidity
             };
 
-            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
-            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
+            let thisDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            let iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${fiveDayResponse.daily[i].weather[0].main}" />`;
 
             // displays the date
             // an icon representation of weather conditions
             // the temperature
             // the humidity
-            var fiveDayCard = $(
-            `<div class="col s6 m2 l2">
+            let fiveDayCard = $(
+            `<div class="col s6 m2 l2 forecast-card">
                 <div class="card cyan darken-4">
                     <div class="card-content white-text">
-                        <span class="card-title">${currDate}</span>
+                        <span class="card-title">${thisDate}</span>
                         <p>${iconURL}</p>
                         <p>Temp: ${cityInfo.temp} °F</p>
                         <p>Humidity: ${cityInfo.humidity}\%</p>
@@ -124,55 +124,57 @@ function fiveDayForecast (latitude, longitude){
 
 }
 
-    const handleErrors = (response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response;
+// const searchBtn = document.getElementById("search-btn");
+$("#search-btn").on("click", function(event) {
+    event.preventDefault();
+
+    let city = $("#city-search-input").val().trim();
+    callWeather(city);
+    if (!searchHistory.includes(city)) {
+        searchHistory.push(city);
+        let searchedCity = $(`
+            <li class="search-history-item">${city}</li>
+            `);
+        $("#search-history").append(searchedCity);
+    };
+    
+    localStorage.setItem("city", JSON.stringify(searchHistory));
+    console.log(searchHistory);
+});
+
+$(document).on("click", ".search-history-item", function() {
+    let pastCity = $(this).text();
+    callWeather(pastCity);
+});
+
+$(document).ready(function() {
+    let searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+
+    if (searchHistoryArr !== null) {
+        let lastSearchedIndex = searchHistoryArr.length - 1;
+        let lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+        callWeather(lastSearchedCity);
+        console.log(`Last searched city: ${lastSearchedCity}`);
     }
+});
 
-    let thisDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
 
-function apiGroup(){
-    let cityInputText = searchInputBox.value;
-    console.log("Button Clicked")
-    .then(searchInputBox.empty())
-    getCity()
-    console.log(latitude + "::" + longitude)
 
-    // get5Day()
-    // getCityInfo()
-    // getTeleportImage()
-};
 
-function submitted(event) {
-    event.preventDefault()
-    apiGroup
-};
 
-const searchBtn = document.getElementById("search-btn");
-searchBtn.addEventListener("click", submitted);
+// const handleErrors = (response) => {
+//     if (!response.ok) {
+//         throw Error(response.statusText);
+//     }
+//     return response;
+// }
 
-function fiveDayCall(lat, lon){
 
-        
+// function getTeleportImage() {
 
-        .then(function(response) {
-            return response.json()
-        })
+//     fetch(`https://api.teleport.org/api/urban_areas/slug:` + searchedCity)
 
-        .then(function(data) {
-            console.log(data)
-        })
-};
-
-const cityInputText = "london"
-
-function getTeleportImage(){
-
-    fetch(`https://api.teleport.org/api/urban_areas/slug:` + searchedCity)
-
-    .then(function(response) {
-        return response.json()
-    })
-};
+//     .then(function(response) {
+//         return response.json()
+//     })
+// };
